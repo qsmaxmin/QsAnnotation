47819b19332f8569203271ad6dd75c2e49ae3df5
qsmaxmin <871245760@qq.com>
2020-08-12 14:07:52 +0800

    基础注解项目

---
+++ app/src/main/java/com/qsmaxmin/annotation/properties/AutoProperty.java
@@ -0,0 +1,16 @@
+package com.qsmaxmin.annotation.properties;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @CreateBy administrator
+ * @Date 2020/8/12 14:06
+ * @Description enable auto generate code
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.CLASS)
+public @interface AutoProperty {
+}
