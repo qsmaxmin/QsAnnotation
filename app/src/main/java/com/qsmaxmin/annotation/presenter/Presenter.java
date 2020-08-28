b5ef60fd40b701413ec8d671e716833c36da5acb
qsmaxmin <871245760@qq.com>
2020-08-28 16:00:40 +0800

    基础注解项目

---
+++ app/src/main/java/com/qsmaxmin/annotation/presenter/Presenter.java
@@ -0,0 +1,20 @@
+package com.qsmaxmin.annotation.presenter;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+/**
+ * @CreateBy qsmaxmin
+ * @Date 2018/7/23 10:18
+ * @Description bind presenter
+ */
+@Documented
+@Target(TYPE)
+@Retention(CLASS)
+public @interface Presenter {
+    Class<?> value();
+}
--- app/src/main/java/com/qsmaxmin/annotation/properties/AutoProperty.java
+++ app/src/main/java/com/qsmaxmin/annotation/properties/AutoProperty.java
@@ -1,5 +1,6 @@
 package com.qsmaxmin.annotation.properties;
 
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -10,6 +11,7 @@ import java.lang.annotation.Target;
  * @Date 2020/8/12 14:06
  * @Description enable auto generate code
  */
+@Documented
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.CLASS)
 public @interface AutoProperty {
