f336cd0f94e5796d04dd9eebf26713296758eeb5
qsmaxmin <871245760@qq.com>
2021-08-26 13:16:22 +0800

    新增@Route注解

---
+++ app/src/main/java/com/qsmaxmin/annotation/route/Route.java
@@ -0,0 +1,19 @@
+package com.qsmaxmin.annotation.route;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @CreateBy qsmaxmin
+ * @Date 2021/8/26 13:12
+ * @Description Route
+ */
+@Documented
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.CLASS)
+public @interface Route {
+    String value();
+}
